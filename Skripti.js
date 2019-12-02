c3a62695a83598eba56611c6b72a30a218570053
Aleksi Aalto <aleksi.aalto@student.hamk.fi>
2019-12-02 11:28:29 +0200

    Versio4

--- Skripti.js
+++ Skripti.js
@@ -1,11 +1,15 @@
-let tietokoneChoice;
-let pelaajaChoice;
+let pelaajanPisteet = 0;
+let tietokoneenPisteet = 0;
 //liitetään html elementit muuttujiin
 let kiviValinta = document.getElementById("kiviBtn");
 let paperiValinta = document.getElementById("paperiBtn");
 let saksiValinta = document.getElementById("saksetBtn");
-let valintaKuva = document.getElementById("valintakuva")
 
+let valintaKuva = document.getElementById("valintakuva")
+let voittaja = document.getElementById("voittaja")
+let pisteet = document.getElementById("pisteet")
+let valinnat = document.getElementById("valinnat")
+let otsikko = document.getElementById("otsikko")
 //peli alustetaan
 
 
@@ -21,9 +25,10 @@ saksiValinta.addEventListener("click", function(){
     pelaajanValinta("Sakset")
 });
 
-function pelaajanValinta(valinta){
+function pelaajanValinta(pelaajaChoice){
     let tietokoneChoice = tietokoneenValinta();
-    console.log("Pelaaja valitsi " + valinta + " : " + "Tietokone valitsi " + tietokoneChoice);
+    console.log("Pelaaja valitsi " + pelaajaChoice + " : " + "Tietokone valitsi " + tietokoneChoice);
+    valintojenVertaus(pelaajaChoice, tietokoneChoice);
 }
 function tietokoneenValinta(){
     let numero = Math.floor(Math.random() * 3);
@@ -37,4 +42,50 @@ function tietokoneenValinta(){
         valintaKuva.src = "sakset.jpg";
         return "Sakset"
     }
+}
+function valintojenVertaus(pvalinta, tvalinta){
+    if (pvalinta === tvalinta){
+        voittaja.innerHTML = "Tasapeli!"
+    } else if (pvalinta === "Kivi"){
+        if (tvalinta === "Sakset"){
+            voittaja.innerHTML = "Kivi voitti. Pelaaja voittaa!<br>Pelaajalle piste."
+            getPisteet(1, 0);
+        } else {
+            voittaja.innerHTML = "Paperi voitti. Tietokone voittaa!<br>Tietokoneelle piste."
+            getPisteet(0, 1);
+        }
+    } else if (pvalinta === "Paperi"){
+        if (tvalinta === "Kivi"){
+            voittaja.innerHTML = "Paperi voitti. Pelaaja voittaa!<br>Pelaajalle piste."
+            getPisteet(1, 0);
+        } else {
+            voittaja.innerHTML = "Sakset voitti. Tietokone voittaa!<br>Tietokoneelle piste."
+            getPisteet(0, 1);
+        }
+    } else if (pvalinta === "Sakset"){
+        if (tvalinta === "Paperi"){
+            voittaja.innerHTML = "Sakset voitti. Pelaaja voittaa!<br>Pelaajalle piste."
+            getPisteet(1, 0);
+        } else {
+            voittaja.innerHTML = "Kivi voitti. Tietokone voittaa!<br>Tietokoneelle piste."
+            getPisteet(0, 1);
+        }
+    }
+} 
+function getPisteet(pPisteet, tPisteet){
+    pelaajanPisteet += pPisteet;
+    tietokoneenPisteet += tPisteet;
+    pisteet.innerHTML = "Pelaajan pisteet: " + pelaajanPisteet + "<br>Tietokoneen pisteet: " + tietokoneenPisteet
+    if (pelaajanPisteet > 2 || tietokoneenPisteet > 2){
+        if (pelaajanPisteet >= tietokoneenPisteet){
+            valinnat.innerHTML = "Pelaaja voitti pelin!"
+            voittoNakyma("Pelaaja voitti pelin!");
+        } else {
+            valinnat.innerHTML = "Tietokone voitti pelin!"
+            voittoNakyma("Tietokone voitti pelin!");
+        }
+    }
+}
+function voittoNakyma(voittoteksti){
+    otsikko.innerHTML = voittoteksti;
 }
